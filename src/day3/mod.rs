bbcf11899f33a5008088a93cf23c25bae99aa57f
Michal Ziemski <mziemski@ethz.ch>
2022-12-07 09:56:27 +0100

    Day 3: part 1

--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -1,15 +1,15 @@
+use array_tool::vec::Intersect;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::iter::{Iterator, Zip};
-use std::slice::Iter;
+use std::iter::{Iterator};
 
 pub fn solve(fp: &str) -> i32 {
-    let (player1, player2) = read_file(fp);
-    process_list(player1, player2)
+    let result = read_file(fp);
+    process_list(result)
 }
 
-fn read_file(fp: &str) -> (Vec<char>, Vec<char>) {
+fn read_file(fp: &str) -> Vec<String> {
     println!("Reading in data from {}", fp);
     let file = File::open(fp).unwrap();
     let reader = BufReader::new(file);
@@ -19,69 +19,36 @@ fn read_file(fp: &str) -> (Vec<char>, Vec<char>) {
         let l = line.unwrap();
         contents.push(l);
     }
+    contents
+}
 
-    let player1 = contents
+fn process_list(backpacks: Vec<String>) -> i32 {
+    let mut letters = "abcdefghijklmnopqrstuvwxyz".to_owned();
+    letters.push_str(&letters.to_uppercase());
+
+    let mut priorities = [0; 52];
+    for (i, v) in priorities.iter_mut().enumerate() {
+        *v = i + 1 as usize
+    }
+
+    let mut priorities_map = HashMap::new();
+    let chars = letters.chars().collect::<Vec<char>>();
+    for (i, letter) in chars.iter().enumerate() {
+        priorities_map.insert(letter, priorities[i]);
+    }
+
+    let mut common_items: Vec<char> = vec![];
+    for bp in backpacks.iter() {
+        let bp_size = bp.len();
+        let c1 = &bp[..bp_size / 2].chars().collect::<Vec<char>>();
+        let c2 = &bp[bp_size / 2..].chars().collect::<Vec<char>>();
+        let common_item = c1.intersect(c2.to_vec());
+        common_items.push(common_item[0])
+    }
+
+    let item_priorities = common_items
         .iter()
-        .map(|x| x.chars().nth(0).unwrap())
-        .collect::<Vec<char>>();
-    let player2 = contents
-        .iter()
-        .map(|x| x.chars().last().unwrap())
-        .collect::<Vec<char>>();
-
-    (player1, player2)
-}
-
-fn is_winner(a: &char, b: &char) -> bool {
-    if (a == &'A' && b == &'Z') || (a == &'C' && b == &'Y') || (a == &'B' && b == &'X') {
-        true
-    } else {
-        false
-    }
-}
-
-fn is_draw(a: &char, b: &char) -> bool {
-    if (a == &'A' && b == &'X') || (a == &'B' && b == &'Y') || (a == &'C' && b == &'Z') {
-        true
-    } else {
-        false
-    }
-}
-
-fn pick_strategy<'a>(players_move: &'a char, desired_outcome: &'a char) -> &'a char {
-    let strategies: HashMap<(&char, &char), &char> = HashMap::from([
-        ((&'A', &'X'), &'Z'),
-        ((&'A', &'Y'), &'X'),
-        ((&'A', &'Z'), &'Y'),
-        ((&'B', &'X'), &'X'),
-        ((&'B', &'Y'), &'Y'),
-        ((&'B', &'Z'), &'Z'),
-        ((&'C', &'X'), &'Y'),
-        ((&'C', &'Y'), &'Z'),
-        ((&'C', &'Z'), &'X'),
-    ]);
-    strategies.get(&(players_move, desired_outcome)).unwrap()
-}
-
-fn process_list(p1: Vec<char>, p2: Vec<char>) -> i32 {
-    let points: HashMap<char, i32> =
-        HashMap::from([('A', 1), ('B', 2), ('C', 3), ('X', 1), ('Y', 2), ('Z', 3)]);
-    let mut p1_points = 0;
-    let mut p2_points = 0;
-
-    for (i, (m1, m2)) in p1.iter().zip(p2.iter()).enumerate() {
-        let strategy = pick_strategy(m1, m2);
-        p1_points += points.get(m1).unwrap();
-        p2_points += points.get(strategy).unwrap();
-
-        if is_winner(m1, strategy) {
-            p1_points += 6
-        } else if is_draw(m1, strategy) {
-            p1_points += 3;
-            p2_points += 3;
-        } else {
-            p2_points += 6
-        }
-    }
-    p2_points
+        .map(|x| priorities_map.get(x).unwrap())
+        .collect::<Vec<&usize>>();
+    item_priorities.iter().map(|x| **x as i32).sum()
 }
